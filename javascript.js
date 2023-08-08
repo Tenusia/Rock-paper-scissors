d6b5a3a2419b9ace961ed54a919c00e1a996bf82
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-08-08 23:35:35 +0200

    Add UI functionalities
    -Add score system
    -Add results and final results
    -Add reset system

--- javascript.js
+++ javascript.js
@@ -1,96 +1,84 @@
 const choiceArray = ["Rock", "Paper", "Scissors"];
-let playerPoints = 0;
-let computerPoints = 0;
 
 const Rock = document.querySelector('#Rock');
 const Paper = document.querySelector('#Paper');
 const Scissors = document.querySelector('#Scissors');
 const msg = document.querySelector('#message');
 const container = document.querySelector('#container');
-const content = document.createElement('div');
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+const Reset = document.querySelector('#Reset');
+
+const player = document.createElement('div');
+player.classList.add('player');
+container.appendChild(player);
+
+const computer = document.createElement('div');
+computer.classList.add('computer');
+container.appendChild(computer);
+
+const resultText = document.createElement('div');
+resultText.classList.add('resultText');
+container.appendChild(resultText);
+
+const finalResultText = document.createElement('div');
+finalResultText.classList.add('finalResultText');
+results.appendChild(finalResultText);
+
+const playerScore = document.createElement('div');
+playerScore.classList.add('player');
+container.appendChild(playerScore);
+
+const computerScore = document.createElement('div');
+computerScore.classList.add('computer');
+container.appendChild(computerScore);
 
 let choice = null;
 let computerSelection = null;
+let playerPoints = 0;
+let computerPoints = 0;
+let pointLimit = 5;
 
 Rock.addEventListener('click', function (e) {
     choice = e.target.innerText; 
-    computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    
-    //msg.classList.toggle('reveal');
-    content.textContent = choice;
-    
-    
-    playRound(choice, getComputerChoice());
-    
-
+    let results = playRound(choice);
+    showResults(results);
+    finalWinner(playerPoints, computerPoints);
 });
 
 Paper.addEventListener('click', function (e) {
     choice = e.target.innerText; 
-    //msg.classList.toggle('reveal');
-    content.textContent = choice;
+    let results = playRound(choice);
+    showResults(results);
+    finalWinner(playerPoints, computerPoints);
 });
 
 Scissors.addEventListener('click', function (e) {
     choice = e.target.innerText; 
-    //msg.classList.toggle('reveal');
-    content.textContent = choice;
+    let results = playRound(choice);
+    showResults(results);
+    finalWinner(playerPoints, computerPoints);
 });
 
-content.classList.add('content');
-content.textContent = choice;
-container.appendChild(content);
-
-//game(choice);
-
-function game(choice) {
-    //const loopTime = prompt("How many rounds do you want to play?", 5);
-    const loopTime = 5;
-    
-    console.log("-------======== ROCK PAPER SISSORS ========-------");
-    for (let i = 0; i < loopTime; i++) { 
-        console.log("===ROUND " + (i+1) + "===")
-        let computerSelection = getComputerChoice();
-        let playerSelection = choice;
-        let computerWord = choiceArray[computerSelection];
-        let playerWord = choiceArray[playerSelection];
-
-        let results = playRound(playerSelection, computerSelection);
-        showResults(results, playerWord, computerWord);
-        console.log("Player points: " + playerPoints + ", Computer points: " + computerPoints);
-    }
-
-    showFinalWinner(playerPoints,computerPoints);
-}
+Reset.addEventListener('click', function (e) {
+    location.reload();
+});
 
 function getComputerChoice() {
     let compChoiceRandomNR = Math.floor(Math.random() * 3);
     return compChoiceRandomNR;
 }
 
-function getPlayerInput() {
-    let playerInput = prompt("Please enter Rock, Paper or Scissors", "");
-    
-    if(playerInput == "") {
-        console.log("Please enter only Rock, Paper or Scissors. Rock is chosen by default.");
-        return 0;
-    }
-    else {
-        let convertedInput = playerInput[0].toUpperCase() + playerInput.slice(1);
-        
-        if (choiceArray.includes(convertedInput)) {
-            let playerIndex = choiceArray.indexOf(convertedInput);
-            return playerIndex;
-        }
-        else {
-            console.log("Please enter only Rock, Paper or Scissors. Rock is chosen by default.");
-            return 0;
-        }
-    }
-}
+function playRound(choice) {
+    let computerSelection = getComputerChoice();
+    let playerSelection = choiceArray.indexOf(choice);
+    let computerWord = choiceArray[computerSelection];
+    let playerWord = choice;
 
-function playRound(playerSelection, computerSelection) {
+    //player.textContent = 'You chose: ' + choice + ' , Computer chose: ' +computerWord;
+    player.textContent = 'You chose: ' + choice;
+    computer.textContent = 'Computer chose: ' + computerWord;
+
     if (playerSelection === computerSelection) {
         return 1;   
     }
@@ -112,28 +100,36 @@ function playRound(playerSelection, computerSelection) {
     }
 }
     
-function showResults(results, playerWord, computerWord) {
+function showResults(results) {
     if (results === 1) {
-        console.log("No winner. Its a tie!");
+        resultText.textContent = 'No winner. Its a tie!';
     }
     else if (results === 2) {
-        console.log("You win. " + playerWord + " beats " + computerWord);
+        resultText.textContent = 'You win!';
         return playerPoints++;
     }
     else if (results === 3) {
-        console.log("You lose. " + computerWord + " beats " + playerWord);
+        resultText.textContent = 'You lose!';
         return computerPoints++;
     }
 }
 
-function showFinalWinner(playerPoints, computerPoints) {
-    if (playerPoints === computerPoints) {
-        console.log("----- Its a tie! -----");
-    }
-    else if (playerPoints > computerPoints) {
-        console.log("----- Congratulations! You win! -----");
-    }
-    else if (playerPoints < computerPoints) {
-        console.log("----- BUMMER! You lose! -----");
+function finalWinner(playerPoints, computerPoints) {
+    playerScore.textContent = 'Your points: ' + playerPoints;
+    computerScore.textContent = 'Computer points: ' + computerPoints;
+    
+    if(playerPoints >= pointLimit || computerPoints >= pointLimit) {
+        if (playerPoints === computerPoints) {
+            finalResultText.textContent = '----- Its a tie! -----';
+            Reset.classList.toggle('reveal');
+        }
+        else if (playerPoints > computerPoints) {
+            finalResultText.textContent = '----- Congratulations! You won! -----';
+            Reset.classList.toggle('reveal');
+        }
+        else if (playerPoints < computerPoints) {
+            finalResultText.textContent = '----- BUMMER! You lost! -----';
+            Reset.classList.toggle('reveal');
+        }
     }
 }
